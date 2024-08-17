4c03b507b874db9dd42fa98ad46d9e46685d7007
Dennis Kestone <denniskestone@gmail.com>
2024-08-17 09:46:11 +0300

    Delete src directory

--- src/components/Privateroute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Outlet, Navigate } from 'react-router-dom'
-import { isLoggedIn } from '../auth';
-const Privateroute = () => {
-    return  isLoggedIn() ? <Outlet /> : <Navigate to={"/login"}  />
-}
-
-export default Privateroute
--- src/services/category-service.js
+++
@@ -1,7 +0,0 @@
-import { myAxios } from "./helper";
-
-export const loadAllCategories = () => {
-  return myAxios.get(`/categories/`).then((respone) => {
-    return respone.data;
-  });
-};
